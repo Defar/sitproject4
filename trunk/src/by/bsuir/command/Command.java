1807dec1346cbb12fab529609ba46f729ea5e665
Defar <Defar94@mail.ru>
2013-12-27 23:54:33 +0200

    add project v2

---
+++ trunk/src/by/bsuir/command/Command.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package by.bsuir.command;
+
+/**
+ *
+ * @author stud224
+ */
+public interface Command {
+    public void execute();
+    public void undo();
+}
